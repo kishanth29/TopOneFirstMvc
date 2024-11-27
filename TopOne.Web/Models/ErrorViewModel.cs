40612591e12ac80472748fbad98cbc21ebf88923
kishanth29 <139776257+kishanth29@users.noreply.github.com>
2024-11-27 16:11:23 +0530

    Add project files.

---
+++ TopOne.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TopOne.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

c0353d061636b9d9f2f9229e45bec3a1a61d0796
kishanth29 <139776257+kishanth29@users.noreply.github.com>
2024-12-03 14:54:02 +0530

    new one

---
+++ TopOne.Web/Models/Brand.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TopOne.Web.Models
+{
+    public class Brand
+    {
+        [Key]
+        public Guid id  { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        // = string.Empty;
+
+        public int EstablishedYear { get; set; }
+
+        public string? BrandLogo { get; set; }
+        // 39:000
+
+    }
+}
---
+++ TopOne.Web/database/TopOneDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopOne.Web.Models;
+
+namespace TopOne.Web.database
+{
+    public class TopOneDbContext :DbContext
+    {
+        public TopOneDbContext(DbContextOptions <TopOneDbContext> options) : base(options) {
+
+        
+        
+        }
+        public DbSet<Brand> Brand { get; set; }  // Brand it will create a table in database 
+        
+    }
+}
